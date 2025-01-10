1fcc9b36fe8a643cd0d526a5428b723302e97bc9
Austin Kaylor <AustinJKaylor@eaton.com>
2025-01-10 16:56:15 -0500

    Added a third.rs file for the singly-linked stack

---
+++ lists/src/third.rs
@@ -0,0 +1,8 @@
+﻿//! A Persistent Singly-Linked Stack
+//!
+//! This module contains the implementation of a singly-linked list that is
+//! persistent, meaning that it preserves the previous version of the list when
+//! a new element is added or removed.
+//!
+//! # More Information
+//! https://rust-unofficial.github.io/too-many-lists/third.html
